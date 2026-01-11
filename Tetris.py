48761853ff69137aa4f3d51aabea443df1df6119
RoboticRobin20 <thoelro@cronos.be>
2026-01-11 17:42:14 +0100

    Added Grid class and reworked Main

--- Tetris.py
+++ Tetris.py
@@ -7,17 +7,17 @@ class Block:
         self.pos = pygame.Vector2(self.x, self.y)
         self.locked = False
         
-    def draw_block(self):
-        self.block_rect = pygame.Rect(int(self.pos.x * main_game.cellsize + main_game.padding / 2), int(self.pos.y * main_game.cellsize + main_game.padding / 2), main_game.cellsize, main_game.cellsize)
-        pygame.draw.rect(main_game.playfield_surface, "red", self.block_rect)
+    def draw_block(self, grid):
+        self.block_rect = pygame.Rect(int(self.pos.x * grid.cellsize + grid.padding / 2), int(self.pos.y * grid.cellsize + grid.padding / 2), grid.cellsize, grid.cellsize)
+        pygame.draw.rect(grid.playfield_surface, "red", self.block_rect)
         
     def move_block(self, direction):
         # block has to go down each cycle
         if self.locked is False:
             self.pos += direction
         
-    def check_landing(self):
-        if self.pos.y == main_game.cellnumbers_height - 1:
+    def check_landing(self, grid):
+        if self.pos.y == grid.cellnumbers_height - 1:
             self.locked = True
 
 class Tetromino:
@@ -82,10 +82,8 @@ class Shape:
     def __init__(self):
         pass
 
-class Main:
+class Grid:
     def __init__(self):
-        self.block = Block()
-        
         self.cellsize = 40
         self.cellnumbers_height = 20
         self.cellnumbers_width = 10
@@ -96,34 +94,31 @@ class Main:
         self.playfield_surface = pygame.Surface((self.cellnumbers_width * 40 + self.padding, self.cellnumbers_height * 40 + self.padding))
         self.centered_rect = self.playfield_surface.get_rect(center=screen.get_rect().center)
     
-    def update(self):
-        self.block.check_landing()
-        self.block.move_block(pygame.Vector2(0,1))
-    
-    def draw_elements(self):
-        screen.fill(self.background_color)
-        self.playfield_surface.fill(self.playfield_color)
-        self.draw_playfield_grid()
-        self.block.draw_block()
-        self.load_playfield()
-    
-    def load_playfield(self):
-        screen.blit(self.playfield_surface, self.centered_rect)
-        
     def draw_playfield_grid(self):
+        self.playfield_surface.fill(self.playfield_color)
         for row in range(self.cellnumbers_height):
             for col in range(self.cellnumbers_width):
                 self.grid_rect = pygame.Rect(int(col * self.cellsize + self.padding), int(row * self.cellsize + self.padding), self.cellsize - self.padding, self.cellsize - self.padding)
                 pygame.draw.rect(self.playfield_surface, self.background_color, self.grid_rect)
 
-# cellsize = 40
-# cellnumbers_height = 20
-# cellnumbers_width = 10
-
-# background_color = (30, 105, 166)
-# playfield_color = "black"
-# playfield_surface = pygame.Surface((cellnumbers_width * 40, cellnumbers_height * 40))
-# centered_rect = playfield_surface.get_rect(center=screen.get_rect().center)
+class Main:
+    def __init__(self):
+        self.grid = Grid()
+        self.block = Block()
+        
+    def update(self):
+        self.block.check_landing(self.grid)
+        self.block.move_block(pygame.Vector2(0,1))
+    
+    def draw_elements(self):
+        screen.fill(self.grid.background_color)
+        self.grid.draw_playfield_grid()
+        self.block.draw_block(self.grid)
+        self.load_playfield()
+    
+    def load_playfield(self):
+        screen.blit(self.grid.playfield_surface, self.grid.centered_rect)
+        
   
 # pygame setup
 pygame.init()
